73ab6aa726c0441b5112f7a565f1a916104264cc
meow <meowndlicht@gmail.com>
2025-08-02 00:07:43 +0800

    improved error-catching and checks

--- services/error_handling.py
+++ services/error_handling.py
@@ -16,6 +16,17 @@ class Check:
         self.authors = self.data['authors']
 
 
+    """Checks to see if there are any empty values detected in the input
+        
+        RETURNS:
+            bool: True if any empty input values are detected, otherwise False. """
+    @staticmethod
+    def detect_empty_values(values: list[str]):
+        if any(value == "" for value in values):
+            return True
+        return False
+
+
     """ Checks for the existence of an item in the dataset.
         
         NOTE: it is REQUIRED to specify the parameter name before entering the value u want to check 
@@ -34,36 +45,49 @@ class Check:
         if borrow_info in self.borrow:
             return True
         
-        # This only works for the first three data types: accounts, library, and authors
-        for data_type in self.data:
-            if field in self.data[data_type]:
-                return True
-        
-        # Borrow data has a very specific structure (also see: BorrowInfoData class from store_data.py)
-        for book_title in self.borrow:
-            for borrowers in self.borrow[book_title]:
-                if field in self.borrow[book_title][borrowers]:
+        # This only works for the first three datasets: accounts, library, and authors
+        for dataset in self.data.values():
+            for names in dataset.values():
+                if field in names.keys():
+                    return True
+
+        # Borrow data has a very specific structure
+        for title in self.borrow:
+            for borrowers in self.borrow[title]:
+                if field in self.borrow[title][borrowers].keys():
                     return True
         return False
     
 
-    """Checks to see if there are any empty values detected in the input
-        
-        RETURNS:
-            bool: True if any empty input values are detected, otherwise False. """
-    @staticmethod
-    def detect_empty_values(values: list[str]):
-        if any(value == "" for value in values):
-            return True
-        return False
-    
-
-    def valid_field(self, field_type: str):
-        ...
-
-
+    """ Checks if a value that the user has given is valid
+     
+        RETURN:
+            bool: True if valid, False otherwise. """
     def is_valid(self, name=None, email=None, role=None, age=None):
-        ...
+        if not isinstance(name, str):
+            return False
+        elif not isinstance(age, int):
+            return False
+        elif not isinstance(email, str):
+            return False
+        elif isinstance(role, str):
+            return False
+        
+        # age validation
+        elif age < 13 or age > 120:
+            return False
+        
+        # email validation
+        elif '@' not in email:
+            return False
+        elif email.split('@')[1].lower() not in ['gmail.com', 'yahoo.com']:
+            return False
+        
+        # role validation
+        elif role.lower() not in ['librarian', 'member']:
+            return False
+        return True
+        
 
 
 
@@ -72,16 +96,22 @@ class Error(Exception):
     ...
 
 class EmptyValueError(Error):
+    """ if an empty value is detected upon calling a method, this'll be called. """
     ...
 
 class NameTaken(Error):
+    """ if an user's, author's, or book's name has already been taken, this'll be called. """
     ...
 
 class NotFound(Error):
+    """ if an user's, author's, or book's name cannot be found in the database, this'll be called. """
     ...
 
 class InvalidAge(Error):
+    """ if an age value is higher (>125) or lower (<13) than the age limit, this'll be called. """
     ...
 
-class InvalidField(Error):
+class InvalidChange(Error):
+    """ if the user tries to change something in a sensitive field, this'll be called. """
     ...
+
