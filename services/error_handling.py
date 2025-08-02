4b69e75b56692476bd66149d35face0497bc7698
meow <meowndlicht@gmail.com>
2025-08-03 00:35:35 +0800

    fixed major bugs

--- services/error_handling.py
+++ services/error_handling.py
@@ -3,19 +3,6 @@ import json
 
 
 class Check:
-    def __init__(self, file='data/storage.json'):
-        self.file = file
-
-        with open(file, 'r') as f:
-            self.data = json.load(f)
-
-        # Shorthands to ease of data calls
-        self.accounts = self.data['accounts']
-        self.library = self.data['library']
-        self.borrow = self.data['borrow_data']
-        self.authors = self.data['authors']
-
-
     """Checks to see if there are any empty values detected in the input
         RETURNS:
             bool: True if any empty input values are detected, otherwise False. """
@@ -31,26 +18,38 @@ class Check:
         sample: (username = name_of_user). 
         RETURNS:
             bool: True if item found (or exists), otherwise False. """
-    def exists(self, username=None, book_title=None, author_name=None, borrow_info=None, field=None):   
-        if username in self.accounts:
+    @staticmethod
+    def exists(username=None, book_title=None, author_name=None, borrow_info=None, field=None, file='data/storage.json'):
+        # Read and parses the json everytime the method is called, which might decrease performance.
+        with open(file, 'r') as f:
+            data = json.load(f)
+        
+        # Shorthands to ease of data calls
+        accounts = data['accounts']
+        library = data['library']
+        authors = data['authors']
+        borrow = data['borrow_data']
+
+
+        if username in accounts:
             return True
-        if book_title in self.library:
+        if book_title in library:
             return True
-        if author_name in self.authors:
+        if author_name in authors:
             return True
-        if borrow_info in self.borrow:
+        if borrow_info in borrow:
             return True
         
         # This only works for the first three datasets: accounts, library, and authors
-        for dataset in self.data.values():
+        for dataset in data.values():
             for names in dataset.values():
                 if field in names.keys():
                     return True
 
-        # Borrow data has a very specific structure
-        for title in self.borrow:
-            for borrowers in self.borrow[title]:
-                if field in self.borrow[title][borrowers].keys():
+        # borrow dataset has a very specific structure
+        for title in borrow:
+            for borrowers in borrow[title]:
+                if field in borrow[title][borrowers].keys():
                     return True
         return False
     
@@ -58,28 +57,29 @@ class Check:
     """ Checks if a value that the user has given is valid
         RETURN:
             bool: True if valid, False otherwise. """
-    def is_valid(self, name=None, email=None, role=None, age=None):
+    @staticmethod
+    def is_valid(name='unknown', email='sample@gmail.com', role='member', age=18):
         if not isinstance(name, str):
             return False
-        elif not isinstance(age, int):
+        if not isinstance(age, int):
             return False
-        elif not isinstance(email, str):
+        if not isinstance(email, str):
             return False
-        elif isinstance(role, str):
+        if not isinstance(role, str):
             return False
         
         # age validation
-        elif age < 13 or age > 120:
+        if age < 13 or age > 120:
             return False
         
         # email validation
-        elif '@' not in email:
+        if '@' not in email:
             return False
         elif email.split('@')[1].lower() not in ['gmail.com', 'yahoo.com']:
             return False
         
         # role validation
-        elif role.lower() not in ['librarian', 'member']:
+        if role.lower() not in ['librarian', 'member']:
             return False
         return True
         
@@ -92,21 +92,27 @@ class Error(Exception):
 
 class EmptyValueError(Error):
     """ if an empty value is detected upon calling a method, this'll be called. """
-    ...
+    def __init__(self):
+        super().__init__('Emtpy values detected')
 
 class NameTaken(Error):
     """ if an user's, author's, or book's name has already been taken, this'll be called. """
     ...
-
+    
 class NotFound(Error):
     """ if an user's, author's, or book's name cannot be found in the database, this'll be called. """
     ...
 
 class InvalidAge(Error):
     """ if an age value is higher (>125) or lower (<13) than the age limit, this'll be called. """
-    ...
+    def __init__(self):
+        super().__init__("Invalid age. Either too old or too young! ")
 
 class InvalidChange(Error):
     """ if the user tries to change something in a sensitive field, this'll be called. """
+    ...
+
+class InvalidEmail(Error):
+    """ if the user tries to use an email that is not '@gmail.com' or '@yahoo.com', this gets called. """
     ...
 
