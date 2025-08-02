d083eb2a497b724790284151236e939738267ec6
meow <meowndlicht@gmail.com>
2025-08-03 00:46:52 +0800

    idk

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from services import GlobalServices, LibrarianServices, MemberServices
-from data import AccountsData, AuthorsData, LibraryData, BorrowInfoData, DataHandling
+""" This should be the only place where you should run the code, otherside it may behave unexpectedly. """
+
+from data import AccountsData, LibraryData, AuthorsData, BorrowData
+from services import GeneralServices, LibrarianServices, MemberServices
+
 
 
 def run_packages():
-    ...
+    library = LibrarianServices()
+    library.create_user('SampleUser1', 'sampleuser1@gmail.com', 25)
 
     
 
