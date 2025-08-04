374853ac05f5152138cd5b2b8c5f9af9953ee97c
meow <meowndlicht@gmail.com>
2025-08-04 21:42:30 +0800

    reverted exists method to instance method

--- services/error_handling.py
+++ services/error_handling.py
@@ -3,6 +3,18 @@ import json
 
 
 class Check:
+    def __init__(self, file='data/storage.json'):
+        with open(file, 'r') as f:
+            self.data = json.load(f)
+        
+        # Shorthands to ease each dataset calls
+        self.accounts = self.data['accounts']
+        self.library = self.data['library']
+        self.authors = self.data['authors']
+        self.borrow = self.data['borrow_data']
+
+
+
     """Checks to see if there are any empty values detected in the input
         RETURNS:
             bool: True if any empty input values are detected, otherwise False. """
@@ -13,52 +25,13 @@ class Check:
         return False
 
 
-    """ Checks for the existence of an item in the dataset.   
-        NOTE: it is REQUIRED to specify the parameter name before entering the value u want to check 
-        sample: (username = name_of_user). 
-        RETURNS:
-            bool: True if item found (or exists), otherwise False. """
-    @staticmethod
-    def exists(username=None, book_title=None, author_name=None, borrow_info=None, field=None, file='data/storage.json'):
-        # Read and parses the json everytime the method is called, which might decrease performance.
-        with open(file, 'r') as f:
-            data = json.load(f)
-        
-        # Shorthands to ease of data calls
-        accounts = data['accounts']
-        library = data['library']
-        authors = data['authors']
-        borrow = data['borrow_data']
-
-
-        if username in accounts:
-            return True
-        if book_title in library:
-            return True
-        if author_name in authors:
-            return True
-        if borrow_info in borrow:
-            return True
-        
-        # This only works for the first three datasets: accounts, library, and authors
-        for dataset in data.values():
-            for names in dataset.values():
-                if field in names.keys():
-                    return True
-
-        # borrow dataset has a very specific structure
-        for title in borrow:
-            for borrowers in borrow[title]:
-                if field in borrow[title][borrowers].keys():
-                    return True
-        return False
-    
 
     """ Checks if a value that the user has given is valid
         RETURN:
             bool: True if valid, False otherwise. """
     @staticmethod
     def is_valid(name='unknown', email='sample@gmail.com', role='member', age=18):
+        # checks if the arg is accurate to the data type we want
         if not isinstance(name, str):
             return False
         if not isinstance(age, int):
@@ -82,7 +55,37 @@ class Check:
         if role.lower() not in ['librarian', 'member']:
             return False
         return True
+
+
+
+    """ Checks for the existence of an item in the dataset.   
+        NOTE: it is REQUIRED to specify the parameter name before entering the value u want to check 
+        sample: (username = name_of_user). 
+        RETURNS:
+            bool: True if item found (or it exists), otherwise False. """
+    def exists(self, username=None, book_title=None, author_name=None, borrow_info=None, field=None):
+        if username in self.accounts:
+            return True
+        if book_title in self.library:
+            return True
+        if author_name in self.authors:
+            return True
+        if borrow_info in self.borrow:
+            return True
         
+        # This only works for the first three datasets: accounts, library, and authors
+        for dataset in self.data.values():
+            for names in dataset.values():
+                if field in names.keys():
+                    return True
+
+        # borrow dataset has a very specific structure
+        for title in self.borrow:
+            for borrowers in self.borrow[title]:
+                if field in self.borrow[title][borrowers].keys():
+                    return True
+        return False
+
 
 
 
@@ -93,7 +96,7 @@ class Error(Exception):
 class EmptyValueError(Error):
     """ if an empty value is detected upon calling a method, this'll be called. """
     def __init__(self):
-        super().__init__('Emtpy values detected')
+        super().__init__('Empty values detected')
 
 class NameTaken(Error):
     """ if an user's, author's, or book's name has already been taken, this'll be called. """
@@ -113,6 +116,6 @@ class InvalidChange(Error):
     ...
 
 class InvalidEmail(Error):
-    """ if the user tries to use an email that is not '@gmail.com' or '@yahoo.com', this gets called. """
+    """ if the user tries to use an email that does not end in '@gmail.com' or '@yahoo.com', this gets called. """
     ...
 
