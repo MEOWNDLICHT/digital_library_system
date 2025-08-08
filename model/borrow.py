7ed6b48bf87964bf386f17bf40b611ae0fc9b5ed
meow <meowndlicht@gmail.com>
2025-08-08 23:00:27 +0800

    changed the borrow section for model and data layer

--- model/borrow.py
+++ model/borrow.py
@@ -1,7 +1,7 @@
 """ Here is the blueprint for the borrow informations object """
 
 class Borrow:
-    def __init__(self, book_title: str, borrowed_by: str, borrowed_on: str, borrow_deadline: str, returned_on='Unknown', user_status='Active'):
+    def __init__(self, book_title: str, borrowed_by: str, borrowed_on: str, borrow_deadline: str, returned_on='unknown', user_status='active'):
         self.book_title = book_title
         self.borrowed_by = borrowed_by
         self.borrowed_on = borrowed_on
