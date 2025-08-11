b05afe3536b701f03ee3ffe3e4832731068715ee
meow <meowndlicht@gmail.com>
2025-08-12 02:06:28 +0800

    moved display.py to presentation layer

--- services/__init__.py
+++ services/__init__.py
@@ -2,4 +2,3 @@ from .generate_id import generate_unique_id
 from .validation import Check
 from .error import EmptyValueError, NameTakenError, NameNotFoundError, InvalidAgeError, InvalidChangeError, InvalidEmailError, InvalidQuantityError, BookUnavailableError
 from .operations import GeneralServices, LibrarianServices, MemberServices
-from .display import UserInteraction
